60d396845342c64c5c86fe20961ec516e58dcfcb
JayJackson22 <jayjackson.com@gmail.com>
2020-10-12 18:41:24 -0600

    add sql file

---
+++ sql_create_tables.sql
@@ -0,0 +1,40 @@
+CREATE TABLE hotels (
+	address varchar,
+	city varchar,
+	country varchar,
+	name varchar,
+	postalCode varchar,
+	province varchar,
+	user_rating float,
+	user_review varchar,
+	review_title varchar,
+	websites varchar,
+	average_rating float
+);
+CREATE TABLE one_star (
+	name varchar,
+	city varchar,
+	region varchar,
+	postalCode varchar,
+	cuisine varchar,
+	price varchar,
+	url varchar
+);
+CREATE TABLE two_star (
+	name varchar,
+	city varchar,
+	region varchar,
+	postalCode varchar,
+	cuisine varchar,
+	price varchar,
+	url varchar
+);
+CREATE TABLE three_star (
+	name varchar,
+	city varchar,
+	region varchar,
+	postalCode varchar,
+	cuisine varchar,
+	price varchar,
+	url varchar
+);
