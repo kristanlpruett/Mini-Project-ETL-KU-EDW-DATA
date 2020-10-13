adb5058cbe0d78d93dc92090587cbf4e700fda56
Adrian (AKA Andrea) Strecker <67436390+adrianstrecker@users.noreply.github.com>
2020-10-13 03:02:00 -0500

    Update table_queries.sql
    Added queries for each table

--- table_queries.sql
+++ table_queries.sql
@@ -9,3 +9,27 @@ OR h.postalcode = '94103'
 OR h.postalcode = '94306'
 OR h.postalcode = '94559'
 OR h.postalcode = '94599';
+
+SELECT h.name AS "Hotel_Name", h.average_rating, two.name, two.postalcode FROM PUBLIC."two_star" as two
+INNER JOIN PUBLIC."hotels" as h
+ON h.postalcode = two.postalcode
+WHERE h.postalcode = '90210'
+OR h.postalcode = '90401'
+OR h.postalcode = '92130'
+OR h.postalcode = '94102'
+OR h.postalcode = '94103'
+OR h.postalcode = '94306'
+OR h.postalcode = '94559'
+OR h.postalcode = '94599';
+
+SELECT h.name AS "Hotel_Name", h.average_rating, three.name, three.postalcode FROM PUBLIC."three_star" as three
+INNER JOIN PUBLIC."hotels" as h
+ON h.postalcode = three.postalcode
+WHERE h.postalcode = '90210'
+OR h.postalcode = '90401'
+OR h.postalcode = '92130'
+OR h.postalcode = '94102'
+OR h.postalcode = '94103'
+OR h.postalcode = '94306'
+OR h.postalcode = '94559'
+OR h.postalcode = '94599';
