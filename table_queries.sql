5a2a517a83944e883a3ddfc837a462b46578322d
Adrian (AKA Andrea) Strecker <67436390+adrianstrecker@users.noreply.github.com>
2020-10-13 09:29:58 -0500

    Update table_queries.sql

--- table_queries.sql
+++ table_queries.sql
@@ -1,5 +1,6 @@
-SELECT h.name AS "Hotel_Name", h.average_rating, one.name, one.postalcode FROM PUBLIC."one_star" as one
-INNER JOIN PUBLIC."hotels" as h
+SELECT h.name AS "Hotel_Name", h.average_rating AS "Average_Rating", one.name AS "Restaurant_Name", one.postalcode AS "Postal_Code" 
+FROM PUBLIC."one_star" AS one
+INNER JOIN PUBLIC."hotels_nodupes" AS h
 ON h.postalcode = one.postalcode
 WHERE h.postalcode = '90210'
 OR h.postalcode = '90401'
@@ -10,8 +11,9 @@ OR h.postalcode = '94306'
 OR h.postalcode = '94559'
 OR h.postalcode = '94599';
 
-SELECT h.name AS "Hotel_Name", h.average_rating, two.name, two.postalcode FROM PUBLIC."two_star" as two
-INNER JOIN PUBLIC."hotels" as h
+SELECT h.name AS "Hotel_Name", h.average_rating AS "Average_Rating", two.name AS "Restaurant_Name", two.postalcode AS "Postal_Code"
+FROM PUBLIC."two_star" AS two
+INNER JOIN PUBLIC."hotels_nodupes" AS h
 ON h.postalcode = two.postalcode
 WHERE h.postalcode = '90210'
 OR h.postalcode = '90401'
@@ -22,8 +24,9 @@ OR h.postalcode = '94306'
 OR h.postalcode = '94559'
 OR h.postalcode = '94599';
 
-SELECT h.name AS "Hotel_Name", h.average_rating, three.name, three.postalcode FROM PUBLIC."three_star" as three
-INNER JOIN PUBLIC."hotels" as h
+SELECT h.name AS "Hotel_Name", h.average_rating AS "Average_Rating", three.name AS "Restaurant_Name", three.postalcode AS "Postal_Code" 
+FROM PUBLIC."three_star" AS three
+INNER JOIN PUBLIC."hotels_nodupes" AS h
 ON h.postalcode = three.postalcode
 WHERE h.postalcode = '90210'
 OR h.postalcode = '90401'
@@ -33,3 +36,6 @@ OR h.postalcode = '94103'
 OR h.postalcode = '94306'
 OR h.postalcode = '94559'
 OR h.postalcode = '94599';
+
+SELECT three.name FROM PUBLIC."three_star" AS three
+WHERE postalcode = '94599';
